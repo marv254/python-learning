66445350524492093a5f9cf6a311aa9a94174133
Marv_K254 <marvinkorir.mk@gmail.com>
2022-06-14 17:14:33 +0300

    update API request

--- pj - API request to gitlab.py
+++ pj - API request to gitlab.py
@@ -4,3 +4,4 @@ response = requests.get("https://gitlab.com/api/v4/users/marv254/projects")
 my_projects = response.json()
 
 for project in my_projects:
+    print(f"Project Name: {project['name']} Project Url: {project['web_url']}")
